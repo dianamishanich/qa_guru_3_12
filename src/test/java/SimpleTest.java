e9cd3f67944fde1563f0db295368452a19574891
dianamishanich <diana-mishanich@ya.ru>
2021-01-08 00:46:17 +0300

    SimpleTest

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,13 +1,7 @@
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleTest {
-
 
         @Test
         void successTest() {
@@ -20,17 +14,4 @@ public class SimpleTest {
 
         }
 
-        @Test
-        void selenideSearchTest() {
-            open("https://google.com");
-            $(byName("q")).setValue("Selenide").pressEnter();
-            $("html").shouldHave(text("selenide.org"));
-        }
-
-        @Test
-        void selenideSearchYandex() {
-            open("https://yandex.ru");
-            $(byName("text")).setValue("Selenide").pressEnter();
-            $("html").shouldHave(text("selenide.org"));
-        }
 }
